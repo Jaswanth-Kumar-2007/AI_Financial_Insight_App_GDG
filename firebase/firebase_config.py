0fd1180efa8b8d24471f42e14d052cb21c261f5c
Jaswanth Kumar <jaswanthkumarkamireddi@gmail.com>
2026-05-12 09:21:21 +0530

    updated firebase config

--- firebase/firebase_config.py
+++ firebase/firebase_config.py
@@ -8,7 +8,7 @@ if not firebase_admin._apps:
         "type": "service_account",
         "project_id": st.secrets["FIREBASE_PROJECT_ID"],
         "private_key_id": st.secrets["FIREBASE_PRIVATE_KEY_ID"],
-        "private_key": st.secrets["FIREBASE_PRIVATE_KEY"],
+        "private_key": st.secrets["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
         "client_email": st.secrets["FIREBASE_CLIENT_EMAIL"],
         "client_id": st.secrets["FIREBASE_CLIENT_ID"],
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
