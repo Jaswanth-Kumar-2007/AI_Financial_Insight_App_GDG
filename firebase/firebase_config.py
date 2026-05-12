afc03b36508d334305d52cb78d34f567b2ca3964
Jaswanth Kumar <jaswanthkumarkamireddi@gmail.com>
2026-05-12 09:33:41 +0530

    updated firebase config

--- firebase/firebase_config.py
+++ firebase/firebase_config.py
@@ -4,19 +4,20 @@ import streamlit as st
 
 if not firebase_admin._apps:
 
-    cred = credentials.Certificate({
+    cred_dict = {
         "type": "service_account",
         "project_id": st.secrets["FIREBASE_PROJECT_ID"],
         "private_key_id": st.secrets["FIREBASE_PRIVATE_KEY_ID"],
         "private_key": st.secrets["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
         "client_email": st.secrets["FIREBASE_CLIENT_EMAIL"],
         "client_id": st.secrets["FIREBASE_CLIENT_ID"],
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": f"https://www.googleapis.com/robot/v1/metadata/x509/{st.secrets['FIREBASE_CLIENT_EMAIL']}"
-    })
+        "auth_uri": st.secrets["FIREBASE_AUTH_URI"],
+        "token_uri": st.secrets["FIREBASE_TOKEN_URI"],
+        "auth_provider_x509_cert_url": st.secrets["FIREBASE_AUTH_PROVIDER"],
+        "client_x509_cert_url": st.secrets["FIREBASE_CLIENT_CERT"],
+    }
 
+    cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
