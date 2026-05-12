240d94737bc95b4b31531a2ce8a3ecd774ebdff2
Jaswanth Kumar <jaswanthkumarkamireddi@gmail.com>
2026-05-12 10:24:13 +0530

    Updated Stocks API

--- services/compare_service.py
+++ services/compare_service.py
@@ -1,9 +1,39 @@
-import yfinance as yf
+import requests
+import streamlit as st
+import time
+import pandas as pd
+
+API_KEY = st.secrets["FINNHUB_API_KEY"]
 
 
+# -----------------------------
+# GET HISTORICAL DATA
+# -----------------------------
+def get_history(symbol):
+
+    to_time = int(time.time())
+    from_time = to_time - (60 * 60 * 24 * 180)  # 6 months
+
+    url = f"https://finnhub.io/api/v1/stock/candle?symbol={symbol}&resolution=D&from={from_time}&to={to_time}&token={API_KEY}"
+
+    data = requests.get(url).json()
+
+    if data.get("s") != "ok":
+        return None
+
+    df = pd.DataFrame({
+        "Close": data["c"]
+    })
+
+    return df
+
+
+# -----------------------------
+# COMPARE STOCKS
+# -----------------------------
 def compare_stocks(stock1, stock2):
 
-    data1 = yf.Ticker(stock1).history(period="6mo")
-    data2 = yf.Ticker(stock2).history(period="6mo")
+    data1 = get_history(stock1)
+    data2 = get_history(stock2)
 
     return data1, data2
