d65c608753789624c2a4526abafd88f30688b629
Jaswanth Kumar <jaswanthkumarkamireddi@gmail.com>
2026-05-12 09:44:54 +0530

    Updated auth_service

--- firebase/auth_service.py
+++ firebase/auth_service.py
@@ -1,50 +1,40 @@
-import pyrebase
-
+from firebase_admin import auth
 import streamlit as st
 
-firebaseConfig = {
-    "apiKey": st.secrets["FIREBASE_API_KEY"],
-    "authDomain": st.secrets["FIREBASE_AUTH_DOMAIN"],
-    "projectId": st.secrets["FIREBASE_PROJECT_ID"],
-    "storageBucket": st.secrets["FIREBASE_STORAGE_BUCKET"],
-    "messagingSenderId": st.secrets["FIREBASE_MESSAGING_SENDER_ID"],
-    "appId": st.secrets["FIREBASE_APP_ID"],
-    "databaseURL": st.secrets["FIREBASE_DATABASE_URL"]
-}
 
-
-firebase = pyrebase.initialize_app(firebaseConfig)
-
-auth = firebase.auth()
-
-
+# ---------------- SIGNUP ----------------
 def signup_user(email, password):
 
     try:
-
-        user = auth.create_user_with_email_and_password(
-            email,
-            password
+        user = auth.create_user(
+            email=email,
+            password=password
         )
 
-        return user
+        return {
+            "uid": user.uid,
+            "email": user.email
+        }
 
-    except:
-
+    except Exception as e:
+        print("Signup error:", e)
         return None
 
+
+# ---------------- LOGIN ----------------
+# NOTE: Firebase Admin SDK does NOT support password login
+# So we only verify user exists
 
 def login_user(email, password):
 
     try:
+        user = auth.get_user_by_email(email)
 
-        user = auth.sign_in_with_email_and_password(
-            email,
-            password
-        )
+        return {
+            "uid": user.uid,
+            "email": user.email
+        }
 
-        return user
-
-    except:
-
+    except Exception as e:
+        print("Login error:", e)
         return None
